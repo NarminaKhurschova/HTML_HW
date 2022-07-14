178005bfa4e6e874dc80e2812715c64381949805
narmina <namira.lir@gmail.com>
2022-07-14 16:19:47 +0400

    Добавление комментариев

--- all/second part of HW - App/main.py
+++ all/second part of HW - App/main.py
@@ -9,22 +9,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def all_candidates():
+    """Роут отображения всех кандидатов"""
     items = load_candidates_from_json()
     return render_template('list.html', items=items)
 
 @app.route("/candidate/<int:x>")
 def profile(x):
+    """Роут отображения кандидата по его id"""
     candidate = get_candidate(x)
     return render_template('single.html', candidate=candidate)
 
 @app.route("/search/<candidate_name>")
 def search_by_name(candidate_name):
+    """Роут отображения кандидата по имени"""
     candidates = find_candidates_by_name(candidate_name)
     count = len(candidates)
     return render_template('search.html', candidates=candidates, count=count )
 
 @app.route("/skill/<skill_name>")
 def search_by_skill(skill_name):
+    """Роут отображения кандидата по скиллу"""
     candidates = get_candidates_by_skills(skill_name)
     count = len(candidates)
     return render_template('skill.html', candidates=candidates, count=count)
--- all/second part of HW - App/utils.py
+++ all/second part of HW - App/utils.py
@@ -1,26 +1,34 @@
 import json
 def load_candidates_from_json():
+    """Функция выгрузки данных кандидатов"""
     with open("candidates.json") as file:
         canditates_data = json.load(file)
         return canditates_data
 
 def get_candidate(candidate_id):
+    """Функция поиска кандидата по id"""
     for candidate in load_candidates_from_json():
         if candidate['id'] == candidate_id:
             return candidate
 
 def find_candidates_by_name(candidate_name):
+    """Функция поиска кандидата по имени"""
     candidates = []
     for candidate in load_candidates_from_json():
+        # Приведение имени и фамилии к удобному сравнению с переменной candidate_name
         name_surname = candidate["name"].lower().split(' ')
+        # Добавление в список подходящего кандидата
         if candidate_name.lower() in name_surname:
             candidates.append(candidate)
     return candidates
 
 def get_candidates_by_skills(candidate_skill):
+    """Функция поиска кандидата по скиллу"""
     candidates_list = []
+    # Приведение списка скиллов к удобному сравнению
     for candidate in load_candidates_from_json():
         skills = candidate['skills'].lower().replace(" ","").split(",")
+        # Добавление подходящего кандидата по совпедению скилла
         if candidate_skill.lower() in skills:
             candidates_list.append(candidate)
     return candidates_list
